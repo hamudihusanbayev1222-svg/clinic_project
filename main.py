7a32ecc18d0ba89a3187759fe5ae0cb2a67406f4
Hamudi <hamudihusanbayev1222@gmail.com>
2026-01-14 12:44:57 +0500

    Doctor va Appointment

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ class Patient:
         self.name = name
         self.age
 
+class Doctor:
+    def __init__(self, name, specialty):
+        self.name = name
+        self.specialty = specialty
+
+class Appointment:
+    def __init__(self, date, patient, doctor):
+        self.date = date
+        self.patient = patient
+        self.doctor = doctor
+
+    def show_appointment(self):
+        print(f"Sana: {self.date}")
+        print(f"Bemor: {self.patient.name}, {self.patient.age} yosh")
+        print(f"Shifokor: {self.doctor.name}, {self.doctor.specialty}")
+        print()
